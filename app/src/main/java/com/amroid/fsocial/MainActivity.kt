942504be83e7eb22b416f0b79193a2da9f98ebbb
Amr sayed <amrsaid@gmail.com>
2023-04-07 02:54:34 +0200

    add compose dependency

--- app/src/main/java/com/amroid/fsocial/MainActivity.kt
+++ app/src/main/java/com/amroid/fsocial/MainActivity.kt
@@ -2,10 +2,14 @@ package com.amroid.fsocial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContent {
+      Text(text = "hello")
+    }
   }
 }
